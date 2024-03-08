cec314da6b67434b56c04e67e57c5d37402bbb63
MUSTAFA ÇİFTÇİ <mustafa471@tsomtal.com>
2024-03-08 10:07:20 +0300

    Initial commit

---
+++ app/src/main/java/tr/com/mustafa/sayfa214sirazsizde/MainActivity.java
@@ -0,0 +1,46 @@
+package tr.com.mustafa.sayfa214sirazsizde;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editTextNumara,editTextAdSoyad;
+    Button buttonEkle;
+    ListView listViewListe;
+    ArrayList<String> ogrenciArrayList= new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ogrenciArrayList);
+        editTextNumara=findViewById(R.id.editTextNumara);
+        editTextAdSoyad=findViewById(R.id.editTextAdSoyad);
+        buttonEkle=findViewById(R.id.buttonEkle);
+        listViewListe=findViewById(R.id.listViewListe);
+        listViewListe.setAdapter(adapter);
+        buttonEkle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Ogrenci ogrenci=new Ogrenci();
+                ogrenci.setNumara(Integer.parseInt(editTextNumara.getText().toString()));
+                ogrenci.setAdSoyad(editTextAdSoyad.getText().toString());
+                ogrenciArrayList.add(ogrenci.getNumara() + " " + ogrenci.getAdSoyad());
+                adapter.notifyDataSetChanged();
+            }
+        });
+        listViewListe.setOnItemClickListener((adapterView, view, i, l) -> {
+            ogrenciArrayList.remove(i);
+            adapter.notifyDataSetChanged();
+        });
+    }
+}
---
+++ app/src/main/java/tr/com/mustafa/sayfa214sirazsizde/Ogrenci.java
@@ -0,0 +1,22 @@
+package tr.com.mustafa.sayfa214sirazsizde;
+
+public class Ogrenci {
+    public int getNumara() {
+        return numara;
+    }
+
+    public void setNumara(int numara) {
+        this.numara = numara;
+    }
+
+    public String getAdSoyad() {
+        return adSoyad;
+    }
+
+    public void setAdSoyad(String adSoyad) {
+        this.adSoyad = adSoyad;
+    }
+
+    private int numara;
+    private String adSoyad;
+}
